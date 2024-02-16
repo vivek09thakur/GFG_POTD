90ae745e66c17a3ceeb6bd464575f31ac5b700fb
Vivek Thakur <me.vivek.thakur09@gmail.com>
2024-02-17 01:58:06 +0530

    Create day17.cpp

---
+++ 2024/Feb/day17.cpp
@@ -0,0 +1,16 @@
+class Solution{
+    public:
+    bool isMaxHeap(int arr[], int n)
+    {
+        // Your code goes here
+         for(int i=0;i<=(n-2)/2;i++) {
+            if(arr[2*i+1] > arr[i]) {
+                return false;
+            }
+            else if(2*i+2<n && arr[2*i+2] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
