1528754acd59fdad792bc8b8d85281a739b049d1
Vivek Thakur <me.vivek.thakur09@gmail.com>
2024-03-08 11:59:38 +0530

    Create day08.cpp

---
+++ 2024/March/day08.cpp
@@ -0,0 +1,32 @@
+class Solution{
+public:	
+	bool sameFreq(string str)
+	{
+	    // code here 
+	    unordered_map<char, int> mp;
+	    unordered_map<int, int> mp2;
+	    for(auto &c: str)mp[c]++;
+	    for(auto &it: mp)
+	    {
+	        mp2[it.second]++;
+	    }
+	    
+	    if(mp2.size()==1)return true;
+	    if(mp2.size()>2)return false;
+	    pair<int, int> a, b;
+	    int i=0;
+	    for(auto &it: mp2)
+	    {
+	        if(!i)a=it;
+	        else b=it;
+	        i++;
+	    }
+	    
+	    if(a.first>b.first)swap(a,b);
+	    
+	    if(a.first==1)return true;
+	    if(b.second!=1)return false;
+	    
+	    return b.first-a.first==1;
+	}
+};
